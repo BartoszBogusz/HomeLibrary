a6d1ed95ad44b55d4beeb50bc2588f8a00d665d0
nithaltak <42481119+nithaltak@users.noreply.github.com>
2019-03-11 10:07:54 +0100

    Added book entity.

---
+++ HomeLibrary/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeLibrary.Entities
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
+        public int Year { get; set; }
+    }
+}
