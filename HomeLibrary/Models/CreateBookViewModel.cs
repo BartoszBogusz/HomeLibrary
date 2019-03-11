a63c41620b228a05d6be831cebb2094130906f77
nithaltak <42481119+nithaltak@users.noreply.github.com>
2019-03-11 14:45:48 +0100

    Added validation

--- HomeLibrary/Models/CreateBookViewModel.cs
+++ HomeLibrary/Models/CreateBookViewModel.cs
@@ -1,10 +1,16 @@
-﻿namespace HomeLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeLibrary.Models
 {
     public class CreateBookViewModel
     {
+        [Required]
         public string Title { get; set; }
         public string AuthorFirstName { get; set; }
+        [Required]
         public string AuthorLastName { get; set; }
+        [Required]
+        [Range(1800,2019)]
         public int Year { get; set; }
     }
 }
--- HomeLibrary/Models/EditBookViewModel.cs
+++ HomeLibrary/Models/EditBookViewModel.cs
@@ -1,11 +1,17 @@
-﻿namespace HomeLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeLibrary.Models
 {
     public class EditBookViewModel
     {
         public int BookId { get; set; }
+        [Required]
         public string Title { get; set; }
         public string AuthorFirstName { get; set; }
+        [Required]
         public string AuthorLastName { get; set; }
+        [Required]
+        [Range(1800, 2019)]
         public int Year { get; set; }
     }
 }
